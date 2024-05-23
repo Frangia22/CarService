5a2388dcbf9ac33b6649e35c448dd0f6df7fe4d3
Frangia22 <giacherofranc22@gmail.com>
2024-05-23 16:00:29 -0300

    feat: :construction: Install daisy UI and configured

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,9 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-
+function App () {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-center'>Hello World!!</h1>
     </>
   )
 }
---
+++ front/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['emerald', 'light', 'dark'],
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ':root'
+  }
+}
