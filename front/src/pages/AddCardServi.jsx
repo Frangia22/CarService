4fc3890cd99e6a0da16148591b38a974957e728b
Frangia22 <giacherofranc22@gmail.com>
2024-05-24 19:18:06 -0300

    feat: :recycle: Import components created in page home and addCardServi

--- front/src/pages/AddCardServi.jsx
+++ front/src/pages/AddCardServi.jsx
@@ -1,45 +1,7 @@
 import { Link } from 'react-router-dom'
+import { InputText } from '../components/Input'
+import { Checkbox, MultiCheckbox } from '../components/Checkbox'
 
-export function InputText ({ label, placeholder, type, className }) {
-  return (
-    <label className={`form-control w-full mx-auto ${className}`}>
-      <div className='label'>
-        <span className='label-text'>{label}</span>
-      </div>
-      <input type={type} placeholder={placeholder} className='input input-bordered input-sm w-full' />
-    </label>
-  )
-}
-export function Checkbox ({ title, label, value }) {
-  return (
-    <div className='form-control'>
-      <label className='label cursor-pointer'>
-        <span className='label-text font-bold max-w-[3.75rem] md:w-[3.75rem]'>{title}:</span>
-        <span className='label-text min-w-36'>{label}</span>
-        <input type='checkbox' className='checkbox md:mr-8' />
-      </label>
-    </div>
-  )
-}
-export function MultiCheckbox ({ title, label, label2, value }) {
-  return (
-    <div className='form-control'>
-      <label className='label cursor-pointer'>
-        <span className='label-text font-bold max-w-[3.75rem] md:w-[3.75rem]'>{title}:</span>
-        <div className='md:w-[18.55rem]'>
-          <div className='flex justify-between my-2 gap-8'>
-            <span className='label-text min-w-36'>{label}</span>
-            <input type='checkbox' defaultChecked className='checkbox md:mr-8' />
-          </div>
-          <div className='flex justify-between my-2'>
-            <span className='label-text min-w-36'>{label2}</span>
-            <input type='checkbox' defaultChecked className='checkbox md:mr-8' />
-          </div>
-        </div>
-      </label>
-    </div>
-  )
-}
 export function AddCardServi () {
   return (
     <main className='md:w-full my-6'>
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,14 +1,8 @@
 import { Link } from 'react-router-dom'
 import responseApi from '../mocks/res-servicar.json'
 import { useState } from 'react'
-
-export function ItemList ({ title, value }) {
-  return (
-    <li>
-      <p className='text-sm my-4'>{title}: <strong className='text-base mx-4'>{value}</strong></p>
-    </li>
-  )
-}
+import { ServiCardDetail } from '../components/ServiCardDetail'
+import { TableCard } from '../components/TableCard'
 
 export function Home () {
   const [cardSelect, setCardSelect] = useState([])
@@ -39,67 +33,11 @@ export function Home () {
           <Link to='add-card-servi' className='btn btn-sm btn-primary my-4'>Agregar tarjeta</Link>
         </form>
         <div className='overflow-x-auto w-full md:w-1/4 px-4 md:mx-auto'>
-          {
-            hasServiCard
-              ? (
-                <table className='table table-xs md:table-md table-zebra'>
-                  {/* head */}
-                  <thead>
-                    <tr>
-                      <th className='text-neutral dark:text-neutral-content'>Patente</th>
-                      <th className='text-neutral dark:text-neutral-content'>Fecha</th>
-                      <th className='text-neutral dark:text-neutral-content'>Acciones</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {
-                        serviCard.map(card => (
-                          <tr key={card.id}>
-                            <td>{card.patent}</td>
-                            <td>{card.date}</td>
-                            <td><button className='btn btn-sm btn-active btn-link' onClick={() => handleView(card.id)}>Ver</button></td>
-                          </tr>
-                        ))
-                    }
-                  </tbody>
-                </table>
-                )
-              : <p className='text-center'>No hay datos</p>
-          }
+          <TableCard dataRow={serviCard} hasCardServi={hasServiCard} onHandleView={handleView} />
         </div>
         <dialog id='my_modal_1' className='modal'>
           <div className='modal-box'>
-            {
-                cardSelect.map(item => (
-                  <ul key={item.id}>
-                    <li>
-                      <h2 className='text-xl font-bold text-center'>
-                        Servi
-                        <span className='badge badge-neutral badge-lg mx-4'>{item.patent}</span>
-                      </h2>
-                    </li>
-                    <li>
-                      <p className='my-4'>Kilometros: <strong className='mx-4 badge badge-info'>{item.kilometers} KM</strong></p>
-                    </li>
-                    <ItemList title='Fecha' value={item.date} />
-                    <ItemList title='Motor' value={item.motor ? 'Cambio de aceite' : ''} />
-                    <ItemList title='Tipo de aceite' value={item['type-of-oil']} />
-                    <ItemList title='Filtro de aceite' value={item['filter-of-oil'] ? 'Cambio de aceite' : ''} />
-                    <ItemList title='Filtro de aire' value={item['filter-of-air']} />
-                    <ItemList title='Filtro de nafta' value={item['gasolime-filter'] ? 'Cambio' : ''} />
-                    <ItemList title='Filtro de gasoil' value={item['filter-of-gasoil'] ? 'Cambio' : ''} />
-                    <ItemList title='Filtro anti-polen' value={item['filter-anti-polen']} />
-                    <ItemList title='Bujias' value={item.plugs ? 'Cambio' : ''} />
-                    <ItemList title='Caja de velocidad' value={item['box-of-velocity']} />
-                    <ItemList title='Correa de distribución' value={item['timing-belt'] ? 'Cambio' : ''} />
-                    <ItemList title='Liquido de freno' value={item['brake-fluid'] ? 'Nivel' : ''} />
-                    <ItemList title='Pastillas de freno' value={item['brake-pads'] ? 'Cambio' : ''} />
-                    <ItemList title='Alineación' value={item.alignment ? 'Si' : 'No'} />
-                    <ItemList title='Diferencial' value={item.differential ? 'Si' : 'No'} />
-                    <ItemList title='Reemplazo agua motor' value={item['change-engine-water'] ? 'Si' : 'No'} />
-                  </ul>
-                ))
-            }
+            <ServiCardDetail dataRow={cardSelect} />
             <div className='modal-action justify-center'>
               <form method='dialog'>
                 <button className='btn btn-sm btn-accent w-32'>Cerrar</button>
