62eba5f60b085f13529aad721d2c2cbad44adb8b
Frangia22 <giacherofranc22@gmail.com>
2024-05-26 19:57:24 -0300

    fix: :bug: Rename key dataRow getServiCardById in component ServiCardDetail

--- front/src/components/ServiCardDetail.jsx
+++ front/src/components/ServiCardDetail.jsx
@@ -14,21 +14,21 @@ export function ServiCardDetail ({ dataRow }) {
           <p className='my-4'>Kilometros: <strong className='mx-4 badge badge-info'>{item.kilometers} KM</strong></p>
         </li>
         <ItemList title='Fecha' value={item.date} />
-        <ItemList title='Motor' value={item.motor ? 'Cambio de aceite' : ''} />
-        <ItemList title='Tipo de aceite' value={item['type-of-oil']} />
-        <ItemList title='Filtro de aceite' value={item['filter-of-oil'] ? 'Cambio de aceite' : ''} />
-        <ItemList title='Filtro de aire' value={item['filter-of-air']} />
-        <ItemList title='Filtro de nafta' value={item['gasolime-filter'] ? 'Cambio' : ''} />
-        <ItemList title='Filtro de gasoil' value={item['filter-of-gasoil'] ? 'Cambio' : ''} />
-        <ItemList title='Filtro anti-polen' value={item['filter-anti-polen']} />
+        <ItemList title='Motor' value={item.engine ? 'Cambio de aceite' : ''} />
+        <ItemList title='Tipo de aceite' value={item.type_of_oil} />
+        <ItemList title='Filtro de aceite' value={item.filter_of_oil ? 'Cambio de aceite' : ''} />
+        <ItemList title='Filtro de aire' value={item.filter_of_air} />
+        <ItemList title='Filtro de nafta' value={item.gasoline_filter ? 'Cambio' : ''} />
+        <ItemList title='Filtro de gasoil' value={item.filter_of_gasoil ? 'Cambio' : ''} />
+        <ItemList title='Filtro anti-polen' value={item.filter_anti_polen} />
         <ItemList title='Bujias' value={item.plugs ? 'Cambio' : ''} />
-        <ItemList title='Caja de velocidad' value={item['box-of-velocity']} />
-        <ItemList title='Correa de distribución' value={item['timing-belt'] ? 'Cambio' : ''} />
-        <ItemList title='Liquido de freno' value={item['brake-fluid'] ? 'Nivel' : ''} />
-        <ItemList title='Pastillas de freno' value={item['brake-pads'] ? 'Cambio' : ''} />
+        <ItemList title='Caja de velocidad' value={item.box_of_velocity} />
+        <ItemList title='Correa de distribución' value={item.timing_belt ? 'Cambio' : ''} />
+        <ItemList title='Liquido de freno' value={item.brake_fluid ? 'Nivel' : ''} />
+        <ItemList title='Pastillas de freno' value={item.brake_pads ? 'Cambio' : ''} />
         <ItemList title='Alineación' value={item.alignment ? 'Si' : 'No'} />
         <ItemList title='Diferencial' value={item.differential ? 'Si' : 'No'} />
-        <ItemList title='Reemplazo agua motor' value={item['change-engine-water'] ? 'Si' : 'No'} />
+        <ItemList title='Reemplazo agua motor' value={item.change_engine_water ? 'Si' : 'No'} />
       </ul>
     ))
   )
