9787d2dd7c01e6c4b757ffd61130b16731058c53
Frangia22 <giacherofranc22@gmail.com>
2024-05-29 17:18:36 -0300

    fix: :lipstick: change width popups

--- front/src/components/Popups.jsx
+++ front/src/components/Popups.jsx
@@ -1,13 +1,13 @@
 export function Popups ({ showMessage, message, className, onClick, showIcon }) {
   return (
-    <div className={showMessage ? 'toast toast-top toast-center md:toast-bottom md:toast-end md:bottom-10' : 'hidden'}>
+    <div className={showMessage ? 'toast toast-top toast-center md:toast-bottom md:toast-end md:bottom-10 w-full md:w-1/4' : 'hidden'}>
       <div className={`alert ${className}`}>
         {
         showIcon
           ? <svg xmlns='http://www.w3.org/2000/svg' className='stroke-current shrink-0 h-6 w-6' fill='none' viewBox='0 0 24 24'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z' /></svg>
           : <svg onClick={onClick} xmlns='http://www.w3.org/2000/svg' className='stroke-current shrink-0 h-6 w-6' fill='none' viewBox='0 0 24 24'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' /></svg>
         }
-        <span>{message}</span>
+        <span className='text-balance'>{message}</span>
       </div>
     </div>
   )
